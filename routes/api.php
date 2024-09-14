c34bf79ba8f0ae17fd8cc8afbebc3d7748a08be0
Beamanuel Tesfaye <beta-stage@tutanota.com>
2024-09-14 20:39:22 +0300

    Add basic authentication

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,10 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+use App\Http\Controllers\AuthController;
+
+Route::post('/user/register', [AuthController::class, 'register']);
+Route::post('/user/login', [AuthController::class, 'login']);
+Route::post('/user/logout', [AuthController::class, 'logout'])
+    ->middleware('auth:sanctum');
