5b336b14dc8d00f37515e2338b5e52e3777e5f66
Beamanuel Tesfaye <beta-stage@tutanota.com>
2024-09-22 15:59:23 +0300

    Remove pagination from our APIs

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -14,26 +14,11 @@ class CategoryController extends Controller
         return response()->json(Category::all(), 200);
     }
 
-    public function show(Request $request, Category $category)
+    public function show(Category $category)
     {
-        $validator = Validator::make($request->all(), [
-            'page' => 'integer|min:1',
-            'per_page' => 'integer|min:1',
-        ]);
-
-        if ($validator->fails()) {
-            return response()->json([
-                'message' => 'Validation failed',
-                'errors' => $validator->errors(),
-            ], 422);
-        }
-
-        $fields = $validator->validated();
-        $fields['per_page'] = $fields['per_page'] ?? 10;
-
         return response()->json([
             'category' => $category,
-            'products' => $category->products()->paginate($fields['per_page']),
+            'products' => $category->products()->get(),
         ], 200);
     }
 }
